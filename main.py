8bc432f48567508879d95bdabd38938ef512ae0c
izairjr <71236255+izairjr@users.noreply.github.com>
2022-05-28 11:51:39 -0300

    Adicionado funções de popup e validações se o caminho for indormado errado. iniciado soma dos valores.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pathlib
 from datetime import date
 from xml.dom import minidom
 import PySimpleGUI as sg
@@ -7,17 +8,24 @@ import os
 sg.theme('DarkAmber')
 now = str(date.today())
 log =''
-
+total_NF = 0
 valor_nf = []
 lista_nf =[]
 now = date.today()
 i = 0
-
+qtd_nf = 0
 def somanotas(caminho):
-    lista_diretorio = os.listdir(caminho)
+    try:
+        lista_diretorio = os.listdir((caminho))
+    except:
+        sg.popup(f'O caminho informado, {caminho} , não foi encontrado ou não é válido', title= 'Atenção!')
     for index in lista_diretorio:
         print(index)
-        xml = open(f'{caminho}\{index}',encoding='UTF8')
+        qtd_nf =+ 1
+        try:
+            xml = open(f'{caminho}\{index}',encoding='UTF8')
+        except:
+            sg.popup(f'O caminho especificado {caminho},não contém XML para leitura', title='Lê caminho')
         nfe = minidom.parse(xml)
         num_nfe = nfe.getElementsByTagName('nNF')
         v_nfe = nfe.getElementsByTagName('vNF')
@@ -25,7 +33,15 @@ def somanotas(caminho):
             lista_nf.append(num_nfe[0].firstChild.data)
             valor_nf.append(v_nfe[0].firstChild.data)
         except:
-            erros = (f'o arquivo {index}, não é um arquivo válido.')
+            sg.popup (f'O arquivo {index}, não é um arquivo válido.', title='Falha de leitura')
+
+def _caculaNotas():
+        caminho = values['-ent-']
+        window['-OUTPUT-'].update(values['-ent-'])
+        somanotas(caminho)
+        sg.popup(f'A lista  de notas é {str(lista_nf)}', title='Resultado')
+        sg.popup(f'O total das notas somadas é de : R${(str(total_NF))}')
+        window['-result2-'].update(str(lista_nf))
 
 layout = [
           [sg.Text('Digite aqui o caminho:')],
@@ -42,18 +58,10 @@ while True:
 
     if event in (None, 'Exit'):
         break
-
     if event == 'Calcular':
-        caminho=(str(values['-ent-']))
-        if caminho != '':
-            window['-OUTPUT-'].update(values['-ent-'])
-            somanotas(caminho)
-            window['-result1-'].update(values[valor_nf])
-            window['-result2-'].update(values[lista_nf])
-        else:
-            window['-OUTPUT-'].update(values[f' Falha ao ler o diretório {caminho}'])
-
-
+        _caculaNotas()
+    else:
+        break
 
 
 
